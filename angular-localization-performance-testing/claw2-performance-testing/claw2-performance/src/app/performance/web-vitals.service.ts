dfc8c2c150469f31627f7ac3a1955923a004dfea
karol <karol@karol.com>
2025-01-22 09:50:59 -0700

    Added claw localization performance baseline project

---
+++ angular-localization-performance-testing/claw2-performance-testing/claw2-performance/src/app/performance/web-vitals.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { onFCP, onLCP, onTTFB, Metric } from 'web-vitals';
+import { BehaviorSubject } from 'rxjs';
+
+export interface PerformanceMetric {
+  name: string;
+  value: number;
+  rating: 'good' | 'needs-improvement' | 'poor';
+  delta: number;
+  id: string;
+  navigationType: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebVitalsService {
+  private fcpMetric = new BehaviorSubject<PerformanceMetric | null>(null);
+  private lcpMetric = new BehaviorSubject<PerformanceMetric | null>(null);
+  private ttfbMetric = new BehaviorSubject<PerformanceMetric | null>(null);
+  
+  fcpMetric$ = this.fcpMetric.asObservable();
+  lcpMetric$ = this.lcpMetric.asObservable();
+  ttfbMetric$ = this.ttfbMetric.asObservable();
+
+  private logMetric(metricName: string, metric: Metric) {
+    console.log(`${metricName}:`, {
+      name: metric.name,
+      value: metric.value,
+      rating: metric.rating,
+      delta: metric.delta,
+      id: metric.id,
+      navigationType: metric.navigationType
+    });
+  }
+
+  private createMetricObject(metric: Metric): PerformanceMetric {
+    return {
+      name: metric.name,
+      value: metric.value,
+      rating: metric.rating,
+      delta: metric.delta,
+      id: metric.id,
+      navigationType: metric.navigationType
+    };
+  }
+
+  measureFCP() {
+    onFCP(metric => {
+      this.logMetric('FCP', metric);
+      this.fcpMetric.next(this.createMetricObject(metric));
+    });
+  }
+
+  measureLCP() {
+    onLCP(metric => {
+      this.logMetric('LCP', metric);
+      this.lcpMetric.next(this.createMetricObject(metric));
+    });
+  }
+
+  measureTTFB() {
+    onTTFB(metric => {
+      this.logMetric('TTFB', metric);
+      this.ttfbMetric.next(this.createMetricObject(metric));
+    });
+  }
+
+  measureAll() {
+    this.measureFCP();
+    this.measureLCP();
+    this.measureTTFB();
+  }
+}
